8cd327f8d9e46620f0a651a36a7b6d909e4a720c
Hassan <halnamer@email.arizona.edu>
2022-02-22 23:51:48 -0700

    more fixes

--- fetch_data.py
+++ fetch_data.py
@@ -22,19 +22,31 @@ def push_data(img_id= '', img_path=PROCESSED_PATH, img_obj=None, count=0):
     return 0
 
 
-def fetch_data(img_url=IMG_URL, img_path=IMG_PATH, filename=""):
+def fetch_data(img_url=IMG_URL, img_path=IMG_PATH, filename="", f_log=None):
     print(f'--------------fetch images------------')
     filename+='.jpg' #concat extension
     os.makedirs(img_path, exist_ok=True)
     img_name = os.path.join(IMG_PATH, filename)
     print(f'fetching: {img_name}')
-    if(os.path.exists(img_name)): 
-        print(f'already exist: {img_name}')
+    if(filename in f_log): 
+        print(f'already exist: {filename}')
         return
     urllib.request.urlretrieve(img_url, img_name)
+    
+    f_log.append(filename)
+    json.dump(f_log, open('fetched_log.json', 'r+'), indent=2)
+
+def fetch_overhead():
+    processed_log = open('fetched_log.json', 'r+')
+    p_log = json.load(processed_log)
+    return p_log
 
 json_file = open('labels.json')
 json_objs = json.load(json_file)
-def fetch_all():  
+def main():  
+    f_log = fetch_overhead()
     for index, obj in enumerate(json_objs):
-        fetch_data(img_url=obj['Labeled Data'], img_path=IMG_PATH, filename=f'{obj["ID"]}')
+        fetch_data(img_url=obj['Labeled Data'], img_path=IMG_PATH, filename=f'{obj["ID"]}', f_log=f_log)
+
+if __name__ == '__main__':
+    main()
